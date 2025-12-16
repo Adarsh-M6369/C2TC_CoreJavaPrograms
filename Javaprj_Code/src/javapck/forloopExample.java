b5839f81c0ff1521e11708eb47bd48db261cd9d8
adars <adars@adarsh>
2025-12-16 23:38:51 +0530

    new commit

---
+++ Javaprj_Code/src/javapck/forloopExample.java
@@ -0,0 +1,13 @@
+package javapck;
+
+public class forloopExample {
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=1000;i--)
+		{
+			System.out.println("value of i:");
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ Javaprj_Code/src/javapck/javacls_code.java
@@ -0,0 +1,8 @@
+package javapck;
+
+public class javacls_code {
+	public static void main(String[] args) {
+		System.out.println("Hello guys");
+	}
+
+}
