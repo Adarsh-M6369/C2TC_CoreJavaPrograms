b0dfd06fdc51d3e8ce267df71166c5172110dd5d
adars <adars@adarsh>
2025-12-18 10:36:56 +0530

    commit3

---
+++ Javaprj_Code/src/com/tnsif/oops/encapsulation/Encapdemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.oops.encapsulation;
+
+public class Encapdemo
+{
+	public static void main(String[] args) 
+	{
+		oopsconcept_encap obj = new oopsconcept_encap();
+		obj. setSerialNum (101);
+		obj. setName("Pooja"); 
+		obj. setAge (20) ;
+		System. out.println (obj) ;
+	}
+
+}
---
+++ Javaprj_Code/src/com/tnsif/oops/polymorphism/palinmain.java
@@ -0,0 +1,14 @@
+package com.tnsif.oops.polymorphism;
+
+public class palinmain {
+	public static void main(String[] args) {
+
+		System.out.println("Is 121 Palindrome? "+palindromedemo.isPalindrome(121));
+		System.out.println("Is 123 Palindrome? "+palindromedemo.isPalindrome(123));
+		
+		System.out.println("Is CAR Palindrome? "+palindromedemo.isPalindrome("CAR"));
+		System.out.println("Is RACECAR Palindrome? "+palindromedemo.isPalindrome("RACECAR"));
+
+	}
+
+}
